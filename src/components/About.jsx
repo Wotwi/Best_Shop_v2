cfe5584c23c8986fe4676eeb098230c6562db24f
Kuba Wawrzyn <j.wawrzyn99@gmail.com>
2023-05-15 19:43:34 +0200

    finished about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
 
-function About(props) {
+function About() {
     return (
-        <div>About</div>
+        <div className="container">
+            <div className="about__wrapper">
+                <h3 className="about__header">The most popular sales platform in country</h3>
+                <div className="about__info">
+                    <div className="about__img">
+                        <img src="src/assets/Person.svg" alt="" />
+                    </div>
+                    <p className="about__number">100000+</p>
+                    <p className="about__desc">daily entries</p>
+                </div>
+                <div className="about__info">
+                    <div className="about__img about__after-first">
+                        <img src="src/assets/Eye.svg" alt=""/>
+                    </div>
+                    <p className="about__number">2000000+</p>
+                    <p className="about__desc">seen products every day</p>
+                </div>
+                <div className="about__info">
+                    <div className="about__img about__after-second">
+                        <img src="src/assets/Catalog.svg" alt=""/>
+                    </div>
+                    <p className="about__number about__after-third">10000+</p>
+                    <p className="about__desc">added posts daily</p>
+                </div>
+            </div>
+        </div>
     );
 }
 
