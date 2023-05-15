c2efd30a2a55034880c4005788e8513d82bb3d00
Kuba Wawrzyn <j.wawrzyn99@gmail.com>
2023-05-15 23:24:27 +0200

    2 of 3 panels in benefits page

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
 
-function Benefits(props) {
+function Benefits() {
     return (
-        <div>Benefits</div>
+        <div className="container">
+            <div className="benefits__wrapper">
+                <div className="benefits__panel-1">
+                    <div className="benefits__text">
+                        <h3 className="benefits__header">Be always first</h3>
+                        <p className="benefits__content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                    </div>
+                    <div className="benefits__img"><img src="src/assets/Macbook2.png" alt=""/></div>
+                </div>
+                <div className="benefits__panel-2">
+                    <div className="benefits__text">
+                        <h3 className="benefits__header">Your shop is where you are!</h3>
+                        <p className="benefits__content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                    </div>
+                    <div className="benefits__img"><img src="src/assets/iPhone2.png" alt=""/></div>
+                </div>
+                <div className="benefits__panel-3">
+                    <div className="benefits__text">
+                        <h3 className="benefits__header">Increase recognition your brand!</h3>
+                        <p className="benefits__content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                    </div>
+                    <div className="benefits__img"><img src="src/assets/Trumpet.png" alt=""/></div>
+                </div>
+            </div>
+        </div>
     );
 }
 
