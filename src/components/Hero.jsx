4ae06ea4dbb019659f33b46f12f50133a2f4d451
Kuba Wawrzyn <j.wawrzyn99@gmail.com>
2023-05-15 16:33:38 +0200

    finished hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 function Hero(props) {
     return (
-        <div>Hero</div>
+        <div className="container">
+            <div className="hero">
+                <h3 className="hero__header">Sell More!</h3>
+                <p className="hero__content">Open shop on our platform and increase your sales</p>
+                <button className="hero__btn">Open your shop</button>
+                <img src="src/assets/Background.png" alt=""/>
+            </div>
+        </div>
     );
 }
 
