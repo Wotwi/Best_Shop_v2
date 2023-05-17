de0c16c504e6160602041a64653c47a1ad048c93
Kuba Wawrzyn <j.wawrzyn99@gmail.com>
2023-05-17 11:51:01 +0200

    finished footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,18 +2,16 @@ import React from 'react';
 
 function Footer() {
     return (
-        <div className="container">
             <div className="footer__wrapper">
                 <div className="footer__name">
-                    <h3 className="footer__header"></h3>
-                    <p className="footer__copy"></p>
+                    <h3 className="footer__header">BestShop</h3>
+                    <p className="footer__copy">© 2019 BestShop LTD, All Rights Reserved</p>
                 </div>
                 <div className="footer__icons">
                     <img src="src/assets/Twitter.svg" alt="" className="footer__icon"/>
                     <img src="src/assets/Facebook.svg" alt="" className="footer__icon"/>
                 </div>
             </div>
-        </div>
     );
 }
 
