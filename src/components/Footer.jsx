23ceef9efa00999635f70083151578ac7d5a3acf
Kuba Wawrzyn <j.wawrzyn99@gmail.com>
2023-05-17 10:59:52 +0200

    html markup and css selectors for footer page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 
-function Footer(props) {
+function Footer() {
     return (
-        <div>Footer</div>
+        <div className="container">
+            <div className="footer__wrapper">
+                <div className="footer__name">
+                    <h3 className="footer__header"></h3>
+                    <p className="footer__copy"></p>
+                </div>
+                <div className="footer__icons">
+                    <img src="src/assets/Twitter.svg" alt="" className="footer__icon"/>
+                    <img src="src/assets/Facebook.svg" alt="" className="footer__icon"/>
+                </div>
+            </div>
+        </div>
     );
 }
 
