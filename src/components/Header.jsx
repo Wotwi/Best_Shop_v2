96d1f8ac9a2016ad169a5a52259364cbe31eb615
Kuba Wawrzyn <j.wawrzyn99@gmail.com>
2023-05-23 10:36:53 +0200

    rwd for header max width 375px

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 function Header() {
+
+    const handleMenuBtn = () => {
+        console.log("it's work");
+    }
+
     return (
         <div className="header">
             <h2 className="header__logo">BestShop</h2>
@@ -11,6 +16,7 @@ function Header() {
                     <li className="header__list--item">PRICES</li>
                     <li className="header__list--item">CONTACT</li>
                 </ul>
+                <button onClick={handleMenuBtn} className="header__menu"><img src="src/assets/menu.png" alt=""/></button>
             </nav>
         </div>
     );
