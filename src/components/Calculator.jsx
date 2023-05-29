c1f9898895d48b6a16db0ce89366b3385ff6708e
Kuba Wawrzyn <j.wawrzyn99@gmail.com>
2023-05-29 15:50:22 +0200

    finished calculator

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,6 +3,14 @@ import {useState} from "react";
 function Calculator() {
     const [value, setValue] = useState('');
     const [orderValue, setOrderValue] = useState('');
+    const [selectedOption, setSelectedOption] = useState('')
+    const [text, setText] = useState('');
+    const [chosenPackage, setChosenPackage] = useState('');
+    const [accountingChecked, setAccountingChecked] = useState(false);
+    const [terminalChecked, setTerminalChecked] = useState(false);
+    const [accountingPrice, setAccountingPrice] = useState('');
+    const [terminalPrice, setTerminalPrice] = useState('');
+    const [total, setTotal] = useState(0);
 
     const handleInputQuantity = (e) => {
         const inputValue = e.target.value;
@@ -13,6 +21,7 @@ function Calculator() {
         } else {
             setValue(String(intValue));
         }
+        calculateTotal();
     };
 
     const handleInputOrder = (e) => {
@@ -24,6 +33,64 @@ function Calculator() {
         } else {
             setOrderValue(String(intValue));
         }
+        calculateTotal();
+    };
+
+    const handleSelect = (e) => {
+        const selectedValue = e.target.value;
+        setSelectedOption(selectedValue)
+
+        if (selectedValue === "Basic") {
+            setChosenPackage('Basic')
+            setText('$0')
+        } else if (selectedValue === "Professional") {
+            setChosenPackage('Professional')
+            setText('$25')
+        } else if (selectedValue === "Premium") {
+            setChosenPackage('Premium')
+            setText('$60')
+        }
+        calculateTotal();
+    }
+
+    const handleAccountingCheckbox = (e) => {
+        const accountingChecked = e.target.checked;
+        setAccountingChecked(accountingChecked);
+
+        if (accountingChecked) {
+            setAccountingPrice('$35');
+        } else {
+            setAccountingPrice('');
+        }
+        calculateTotal();
+    }
+
+    const handleTerminalCheckbox = (e) => {
+        const terminalChecked = e.target.checked;
+        setTerminalChecked(terminalChecked);
+
+        if (terminalChecked) {
+            setTerminalPrice('$5');
+        } else {
+            setTerminalPrice('');
+        }
+        calculateTotal();
+    }
+
+    const calculateTotal = () => {
+        const productsCost = value * 0.5;
+        const ordersCost = orderValue * 0.25;
+
+        let total = productsCost + ordersCost;
+
+        if (accountingChecked) {
+            total += 35;
+        }
+        if (terminalChecked) {
+            total += 5;
+        }
+
+        setTotal(total);
     };
 
     return (
@@ -52,35 +119,47 @@ function Calculator() {
                     />
 
                     <label htmlFor="package" className="calc__text-label"></label>
-                    <select name="package" id="package" className="calc__text-input" placeholder="Choose package">
+                    <select name="package" id="package" className="calc__text-input" placeholder="Choose package" value={selectedOption} onChange={handleSelect}>
+                        <option className="calc__select-item" value="---">---</option>
                         <option className="calc__select-item" value="Basic">Basic</option>
                         <option className="calc__select-item" value="Professional">Professional</option>
                         <option className="calc__select-item" value="Premium">Premium</option>
                     </select>
 
                     <span className="calc__checkbox-span">
-                        <label htmlFor="accounting" className="calc__checkbox-label"><input type="checkbox" className="calc__checkbox-input" id="accounting"/>Accounting</label>
-                        <label htmlFor="rental" className="calc__checkbox-label"><input type="checkbox" className="calc__checkbox-input" id="rental"/>Rental of a payment terminal</label>
+                        <label htmlFor="accounting" className="calc__checkbox-label"><input type="checkbox" className="calc__checkbox-input" id="accounting" checked={accountingChecked} onChange={handleAccountingCheckbox}/>Accounting</label>
+                        <label htmlFor="rental" className="calc__checkbox-label"><input type="checkbox" className="calc__checkbox-input" id="rental" checked={terminalChecked} onChange={handleTerminalCheckbox}/>Rental of a payment terminal</label>
                     </span>
                 </form>
                 <span className="calc__results">
                     <div className="calc__results-item">
-                        Products
+                        <p>Products</p>
+                        <p>{value + " * 0.5"}</p>
+                        <p>{"$" + value * 0.5}</p>
                     </div>
                     <div className="calc__results-item">
-                        Orders
+                        <p>Orders</p>
+                        <p>{orderValue + " * 0.25"}</p>
+                        <p>{"$" + orderValue * 0.25}</p>
                     </div>
                     <div className="calc__results-item">
-                        Package
+                        <p>Package</p>
+                        <p>{chosenPackage}</p>
+                        <p>{text}</p>
                     </div>
                     <div className="calc__results-item">
-                        Accounting
+                        <p>Accounting</p>
+                        <p>{accountingPrice}</p>
                     </div>
                     <div className="calc__results-item">
-                        Terminal
+                        <p>Terminal</p>
+                        <p>{terminalPrice}</p>
                     </div>
 
-                    <div className="calc__result-total">Total</div>
+                    <div className="calc__result-total">
+                        <p>Total</p>
+                        <p>{"$" + total}</p>
+                    </div>
                 </span>
             </section>
         </div>
